ad022044aa2e65fb034670fac4a12d7eff1be783
André Dutra <adoliveira@protonmail.com>
2020-02-09 18:21:23 -0300

    icon showing in status bar

--- year-progress/AppDelegate.swift
+++ year-progress/AppDelegate.swift
@@ -10,11 +10,28 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var statusItem: NSStatusItem?
 
-
+    @objc func printQuote(_ sender: Any?) {
+      let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+      let quoteAuthor = "Mark Twain"
+      
+      print("\(quoteText) — \(quoteAuthor)")
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        guard let button = statusItem?.button else {
+            print("status bar item failed. Try removing some menu bar item.")
+            NSApp.terminate(nil)
+            return
+        }
+        
+        button.image = NSImage(named: "hourglass")
+        button.target = self
+        button.action = #selector(printQuote(_:))
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
